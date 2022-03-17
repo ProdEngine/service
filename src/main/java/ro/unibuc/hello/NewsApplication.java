116a18ced67c9cb24238dd39fe6eb608b4879f62
Arthurelul <vilceanu.razvan@gmail.com>
2022-03-17 14:38:44 +0200

    solving conflicts

--- src/main/java/ro/unibuc/hello/NewsApplication.java
+++ src/main/java/ro/unibuc/hello/NewsApplication.java
@@ -4,25 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import ro.unibuc.hello.data.*;
+import ro.unibuc.hello.data.NewsRepository;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = NewsRepository.class)
-public class HelloApplication {
+public class NewsApplication {
 
-	@Autowired
-	private InformationRepository informationRepository;
 	@Autowired
 	private NewsRepository newsRepository;
-	@Autowired
-	private DonationRepository donationRepository;
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
+		SpringApplication.run(NewsApplication.class, args);
 	}
 
+	@PostConstruct
+	public void runAfterObjectCreated() {
+		/*informationRepository.deleteAll();
+		informationRepository.save(new InformationEntity("Overview",
+				"This is an example of using a data storage engine running separately from our applications server"));*/
+		// newsRepository.deleteAll();
 
+	}
 
 }
